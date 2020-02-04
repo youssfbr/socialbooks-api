db2d35272ffba70076a5b0529582dfa94e014a0b
Alisson Youssf <youssfbr@gmail.com>
2020-02-04 19:45:36 -0300

    Buscando pelo id

--- src/main/java/com/alissondev/socialbooksapi/controller/LivroController.java
+++ src/main/java/com/alissondev/socialbooksapi/controller/LivroController.java
@@ -1,11 +1,14 @@
 package com.alissondev.socialbooksapi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +28,16 @@ public class LivroController {
 	@GetMapping
 	public List<Livro> listar() {		
 		return livrosRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Livro> buscar(@PathVariable Long id) {
+		Optional<Livro> livro = livrosRepository.findById(id);
+		
+		if (livro.isEmpty()) 
+			return ResponseEntity.notFound().build();		
+		
+		return ResponseEntity.ok(livro.get());
 	}
 	
 	@PostMapping
