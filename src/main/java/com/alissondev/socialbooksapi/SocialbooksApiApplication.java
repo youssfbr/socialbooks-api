24bf69126d0f9d7649a97239633327484822d53a
Alisson Youssf <youssfbr@gmail.com>
2020-02-04 00:00:35 -0300

    Projeto Criado

---
+++ src/main/java/com/alissondev/socialbooksapi/SocialbooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.alissondev.socialbooksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialbooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialbooksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alissondev/socialbooksapi/SocialbooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissondev.socialbooksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialbooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
