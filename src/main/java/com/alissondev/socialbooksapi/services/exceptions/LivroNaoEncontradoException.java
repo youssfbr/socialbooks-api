dc91b00000460f1149a2696ceae1c16fc2a312c2
Alisson Youssf <youssfbr@gmail.com>
2020-02-05 10:39:41 -0300

    Configurando classe de excecao

--- src/main/java/com/alissondev/socialbooksapi/services/exceptions/LivroNaoEncontradoException.java
+++ src/main/java/com/alissondev/socialbooksapi/services/exceptions/LivroNaoEncontradoException.java
@@ -1,5 +1,18 @@
 package com.alissondev.socialbooksapi.services.exceptions;
 
-public class LivroNaoEncontradoException {
+public class LivroNaoEncontradoException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7868778995518841399L;
+
+	public LivroNaoEncontradoException(String mensagem) {
+		super(mensagem);	
+	}
+	
+	public LivroNaoEncontradoException(String mensagem, Throwable causa) {
+		super(mensagem, causa);
+	}
 
 }
