64c466405ac2294a1ac9954f2513b2127e26438d
Alisson Youssf <youssfbr@gmail.com>
2020-02-05 10:30:35 -0300

    Criando método listar() no serviço

--- src/main/java/com/alissondev/socialbooksapi/services/LivrosServices.java
+++ src/main/java/com/alissondev/socialbooksapi/services/LivrosServices.java
@@ -1,5 +1,20 @@
 package com.alissondev.socialbooksapi.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alissondev.socialbooksapi.entity.Livro;
+import com.alissondev.socialbooksapi.repository.LivrosRepository;
+
+@Service
 public class LivrosServices {
 
+	@Autowired
+	private LivrosRepository livrosRepository;
+	
+	public List<Livro> listar() {
+		return livrosRepository.findAll();
+	}	
 }
